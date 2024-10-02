da59ed9a0b7fda114c98efcbaea975e03e574976
YonaYuechen <yona.yuechen@gmail.com>
2024-10-02 12:14:22 -0400

    Revert "for loop"
    This reverts commit 0d192131f671e99aa06cf69dec9b5d7788a2131f.

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -28,12 +28,8 @@ while lineString:
     #Split the string into a list of data items
     lineData = lineString.split()
 
-    #Extract items in list into variables
-    record_id = lineData[0]
-    obs_date = lineData[2]
-    obs_lc = lineData[4]
-    obs_lat = lineData[6]
-    obs_lon = lineData[7]
+#Split the string into a list of data items
+lineData = lineString.split()
 
     #Print the location of sara
     print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
